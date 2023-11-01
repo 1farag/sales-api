a2a9a41e4c8ac341c56ab43a81c7355e073f336e
ahmedelgohary2 <ahmedfarag.acc1@gmail.com>
2023-11-01 17:38:09 +0200

    delete comment

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,17 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { UserController } from './user.controller';
-// import { UserService } from './user.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { UserRepository } from './user.repository';
-// import { UserEntity } from './user.entity/user.entity';
-
-// @Module({
-//   imports: [TypeOrmModule.forFeature([UserEntity])],
-//   providers: [UserService, UserRepository],
-//   controllers: [UserController],
-//   exports: [UserService],
-// })
-// export class UserModule {}
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
